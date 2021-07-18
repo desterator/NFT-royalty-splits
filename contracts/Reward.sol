487570ca094e62ae6bb764f237e233d9e20ece0d
DzenkivV <viktor.dzenkiv@nure.ua>
2021-07-19 00:27:10 +0300

    implement nft contract

--- contracts/Reward.sol
+++ contracts/Reward.sol
@@ -2,8 +2,7 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./IwETH.sol";
+import "./interface/IwETH.sol";
 
 contract Reward is Ownable {
   IwETH constant public wETH = IwETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
@@ -19,13 +18,12 @@ contract Reward is Ownable {
 
   // 50% goes to the dev, 50% goes to the winner
   function distribute(address _winner) external onlyOwner {
-    IwETH _wETH = wETH;
-    uint256 _amount = _wETH.balanceOf(address(this)) / 2;
+    uint256 _amount = wETH.balanceOf(address(this)) / 2;
     if (_amount == 0) {
       return;
     } 
 
-    _wETH.transfer(dev, _amount);
-    _wETH.transfer(_winner, _amount);
+    wETH.transfer(dev, _amount);
+    wETH.transfer(_winner, _amount);
   }
 }
