6615676c49bf18740bda89942235366af75b97eb
Viktor <viktor.dzenkiv@nure.ua>
2021-07-31 16:00:49 +0300

    implement deploy scripts for eth

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const mnemonic = "";
+
 module.exports = {
   networks: {
     development: {
@@ -8,6 +12,32 @@ module.exports = {
       gasLimit: 6721975,
       gasPrice: 1
     },
+    eth: {
+      network_id: "1",
+      provider: () =>
+          new HDWalletProvider(
+               mnemonic,
+              "https://mainnet.infura.io/v3/"
+          ),
+      gasPrice: 10000000000, // 10 gwei
+      gas: 8000000,
+    },
+    ropsten: {
+      network_id: "3",
+      provider: () =>
+          new HDWalletProvider(
+              mnemonic,
+              "https://ropsten.infura.io/v3/"
+          ),
+      gasPrice: 10000000000, // 10 gwei
+      gas: 8000000,
+    },
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: "",
   },
   compilers: {
     solc: {
